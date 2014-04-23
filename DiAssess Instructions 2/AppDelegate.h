5475ea20a99640c16e65545edab70c1e052d7c99
alicehyun <azhyun@gmail.com>
2014-04-23 12:32:26 -0700

    Initial Commit

---
+++ DiAssess Instructions 2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DiAssess Instructions 2
+//
+//  Created by Alice Hyun on 4/23/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DiAssess Instructions 2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DiAssess Instructions 2
+//
+//  Created by Alice Hyun on 4/23/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
